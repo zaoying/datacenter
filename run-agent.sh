15f236fe2ef7cd7a93f93989141ef0a786910973
zurong <505155730@qq.com>
2019-05-31 17:51:01 +0800

    去除无效JENKINS_NODE_COOKIE=dontKillMe变量

--- run-agent.sh
+++ run-agent.sh
@@ -1,4 +1,4 @@
-JENKINS_NODE_COOKIE=dontKillMe nohup java \
+nohup java \
 -javaagent:/home/bocloud/skywalking/agent/skywalking-agent.jar \
 -Dskywalking.agent.application_code=$2 \
 -jar $1 > $2.log &
