2eaf48b73656972a02f76460ff187f51ce53b3e9
zurong <505155730@qq.com>
2019-05-31 15:45:09 +0800

    修改agent参数

--- run-agent.sh
+++ run-agent.sh
@@ -1,4 +1,4 @@
 nohup java \
--javaagent=/home/bocloud/skywalking/agent/skywalking-agent.jar \
+-javaagent:/home/bocloud/skywalking/agent/skywalking-agent.jar \
 -Dagent.application_code=$2 \
 -jar $1 > $2.log &
