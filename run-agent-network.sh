6edfeb47337cd0f38b127efabaebc76ec4a7ff12
zaoying <505155730@qq.com>
2019-06-11 23:41:03 +0800

    新增指定监听地址脚本

---
+++ run-agent-network.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+JENKINS_NODE_COOKIE=dontKillMe \
+nohup java \
+-javaagent:/home/bocloud/skywalking/agent/skywalking-agent.jar \
+-Dskywalking.agent.application_code=$2 \
+-Dserver.address=172.18.0.1 \
+-jar $1 > $2.log & echo $!  >> pid
