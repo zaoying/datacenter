b54f7cbe472046509d93ea29b0db8c14a15f68b8
zurong <505155730@qq.com>
2019-07-23 13:54:05 +0800

    不再指定spring.active.profile

--- run-agent.sh
+++ run-agent.sh
@@ -3,5 +3,4 @@ JENKINS_NODE_COOKIE=dontKillMe \
 nohup java \
 -javaagent:/home/bocloud/skywalking/agent/skywalking-agent.jar \
 -Dskywalking.agent.application_code=$2 \
--Dspring.profiles.active=dev \
 -jar $1 > $2.log & echo $!  >> pid
