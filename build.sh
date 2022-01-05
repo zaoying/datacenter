295212abfa484fa675a6f4e2ba1b581d9e409dce
zaoying <505155730@qq.com>
2022-01-05 22:56:50 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ docker build -t datacenter-eureka:latest eureka
 docker build -t datacenter-school:latest school
 docker build -t datacenter-student:latest student
 docker build -t datacenter-teacher:latest teacher
-docker build -t datacenter-zuul:latest zuul
+# docker build -t datacenter-zuul:latest zuul
 # docker build -t datacenter-zipkin:latest zipkin
